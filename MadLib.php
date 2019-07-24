64e441b7a283881d862fb3108cf118eeaaf9bfec
Ch Ahsan <Chahsan137@gmail.com>
2019-07-24 13:17:45 -0900

    Create MadLib.php
    A simple mad Lib story telling console based program

---
+++ MadLib.php
@@ -0,0 +1,20 @@
+<?php
+  
+  
+//function defination
+  
+  /*
+  *takes the perameters and make funny or 				something like wired story.
+  */
+function generateStory($singular_noun , $verb , $color, $distance_unit)
+{
+  $story = "\nThe $singular_noun are lovely, 		$color, and deep.\nBut I have promises to 		keep,\nAnd $distance_unit to go before I 			$verb,\nAnd $distance_unit to go before I 		$verb.\n";
+  
+  return $story;
+}
+
+//execution
+echo generateStory("monkey", "eat", "brown", "fifty");
+echo generateStory("empty void", "speak", "beige", "four");
+echo generateStory("car", "cook", "vermilion", "nine");
+?>
